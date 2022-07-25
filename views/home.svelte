3563f5f91fd35a3fea914dcf8ee06563fb4b9230
RubberDuckShobe <dog@dog.dog>
2022-07-25 22:59:01 +0200

    Take out the trash

--- views/home.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-    import Navbar from "./components/common/navbar.svelte";
-</script>
-
-<Navbar></Navbar>
-
-<p><strong>Wavebreaker</strong> is a custom server for <a href="https://store.steampowered.com/app/12900/AudioSurf">Audiosurf</a>.</p>
