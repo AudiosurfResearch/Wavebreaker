e525bcf9961b19cbd1f002f6ae458a023c3bac2e
RubberDuckShobe <dog@dog.dog>
2022-07-26 12:05:10 +0200

    Add login w/ placeholder data + get the base ready

--- index.js
+++ index.js
@@ -4,14 +4,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fastify_1 = __importDefault(require("fastify"));
-const server = (0, fastify_1.default)();
-server.listen({ port: 8080 }, (err, address) => {
+const formbody_1 = __importDefault(require("@fastify/formbody"));
+const wavebreaker_config_json_1 = __importDefault(require("./wavebreaker_config.json"));
+const accounts_1 = __importDefault(require("./routes/as1/accounts"));
+const fs_1 = __importDefault(require("fs"));
+const fastify = (0, fastify_1.default)({
+    logger: true,
+    https: {
+        key: fs_1.default.readFileSync(wavebreaker_config_json_1.default.https.key),
+        cert: fs_1.default.readFileSync(wavebreaker_config_json_1.default.https.cert),
+        passphrase: wavebreaker_config_json_1.default.https.passphrase
+    },
+});
+fastify.listen({ port: wavebreaker_config_json_1.default.port }, (err, address) => {
     if (err) {
-        console.error(err);
+        fastify.log.error(err);
         process.exit(1);
     }
-    console.log(`Server listening at ${address}`);
 });
-server.get('/ping', async (request, reply) => {
-    return 'pong\n';
-});
+fastify.register(formbody_1.default); //So we can parse requests that use application/x-www-form-urlencoded
+fastify.register(accounts_1.default);
