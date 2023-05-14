a158c3efcff7161f28fa0841cd36d98a51498a39
RubberDuckShobe <dog@dog.dog>
2023-05-14 12:20:17 +0200

    Pretty logging in dev + make radio serve files

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import Fastify from "fastify";
 import formbody from "@fastify/formbody";
+import fastifyStatic from "@fastify/static";
 import httpsRedirect from "fastify-https-redirect";
 import WavebreakerConfig from "./wavebreaker_config.json";
 import accountsRouter from "./routes/as1/accounts";
@@ -7,14 +8,36 @@ import gameplayRouter from "./routes/as1/gameplay";
 import informationRouter from "./routes/as1/information";
 import radioRouter from "./routes/as1/radio";
 import fs from "fs";
+import path from "path";
+
+//weird hack to select logger based on environment
+const logger = {
+  ...(WavebreakerConfig.environment == "development" && {
+    logger: {
+      transport: {
+        target: "pino-pretty",
+        options: {
+          translateTime: "HH:MM:ss Z",
+          ignore: "pid,hostname",
+        },
+      },
+    },
+  }),
+  ...(WavebreakerConfig.environment != "development" && {
+    logger: true,
+  }),
+};
 
 const fastify = Fastify({
-  logger: WavebreakerConfig.logger,
-  https: {
-    key: fs.readFileSync(WavebreakerConfig.https.key),
-    cert: fs.readFileSync(WavebreakerConfig.https.cert),
-    passphrase: WavebreakerConfig.https.passphrase,
-  },
+  ...logger,
+  //For HTTPS in production, please use nginx or whatever
+  ...(WavebreakerConfig.environment == "development" && {
+    https: {
+      key: fs.readFileSync(WavebreakerConfig.https.key),
+      cert: fs.readFileSync(WavebreakerConfig.https.cert),
+      passphrase: WavebreakerConfig.https.passphrase,
+    },
+  }),
 });
 
 fastify.listen({ port: WavebreakerConfig.port }, (err) => {
@@ -24,8 +47,14 @@ fastify.listen({ port: WavebreakerConfig.port }, (err) => {
   }
 });
 
-fastify.register(httpsRedirect);
+if (WavebreakerConfig.environment == "development")
+  fastify.register(httpsRedirect); //HTTPS redirect for development, please use nginx or whatever for this in prod
+
 fastify.register(formbody); //So we can parse requests that use application/x-www-form-urlencoded
+fastify.register(fastifyStatic, {
+  root: path.join(__dirname, "RadioSongs"),
+  prefix: "/as/asradio/",
+});
 
 fastify.register(accountsRouter);
 fastify.register(gameplayRouter);
--- routes/as1/radio.ts
+++ routes/as1/radio.ts
@@ -1,9 +1,29 @@
 import { FastifyInstance } from "fastify";
+import WavebreakerRadioConfig from "../../wavebreaker_radio_entries.json";
+
+type RadioEntry = {
+  title: string;
+  artist: string;
+  externalUrl: string;
+  cgrFileUrl: string;
+};
 
 export default async function routes(fastify: FastifyInstance) {
-  fastify.get("/as/asradio/game_asradiolist5.php", async (request, response) => {
-    //TODO: proper implementation
-    response.header("Content-Type", "text/html");
-    return "Wavebreaker-:*x-Test Entry-:*x-http://www.audio-surf.com/as/asradio/ASR_IDropGems.cgr-:*x-https://github.com/AudiosurfResearch/Wavebreaker-:*x-";
-  });
+  fastify.get(
+    "/as/asradio/game_asradiolist5.php",
+    async () => {
+      if (WavebreakerRadioConfig.availableSongs.length == 0) return "";
+      
+      const separator = "-:*x-";
+      const radioEntries: RadioEntry[] = WavebreakerRadioConfig.availableSongs;
+
+      //Join every radio entry's properties (and the entries themselves) with the separator
+      const entriesString =
+        radioEntries
+          .map((entry) => Object.values(entry).join(separator))
+          .join(separator) + separator;
+
+      return entriesString;
+    }
+  );
 }
