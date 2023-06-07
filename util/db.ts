b4fc893dce9b0ce6553510d6d4fa854c51b8ff48
RubberDuckShobe <42943070+RubberDuckShobe@users.noreply.github.com>
2023-06-07 19:38:10 +0200

    Log DB query time

--- util/db.ts
+++ util/db.ts
@@ -1,3 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient({
+  log: [
+    {
+      emit: "event",
+      level: "query",
+    },
+  ],
+});
+
+prisma.$on("query", (e) => {
+  console.log("DB query took " + e.duration + "ms");
+});
