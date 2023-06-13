2d7a6826d5a212e8ae6a043a08ec48a42b6c1f7f
RubberDuckShobe <42943070+RubberDuckShobe@users.noreply.github.com>
2023-06-13 18:51:37 +0200

    Add schemas + validation to AS1 account routes

--- routes/as1/accounts.ts
+++ routes/as1/accounts.ts
@@ -1,142 +1,169 @@
 import { FastifyInstance } from "fastify";
 import { Prisma, User } from "@prisma/client";
 import { prisma } from "../../util/db";
+import { Static, Type } from "@sinclair/typebox";
 import * as SteamUtils from "../../util/steam";
 import xml2js from "xml2js";
 import SteamID from "steamid";
 
 const xmlBuilder = new xml2js.Builder();
 
-interface SteamLoginRequest {
-  steamusername: string;
-  snum: number;
-  s64: number;
-  ticket: string;
-}
+const steamLoginRequestSchema = Type.Object(
+  {
+    steamusername: Type.String(),
+    snum: Type.Number(),
+    s64: Type.String(),
+    ticket: Type.String(),
+  },
+  { additionalProperties: false }
+);
+type SteamLoginRequest = Static<typeof steamLoginRequestSchema>;
 
-interface UpdateLocationRequest {
-  s64: number;
-  ticket: string;
-  locationid: number;
-}
+const updateLocationRequestSchema = Type.Object(
+  {
+    s64: Type.String(),
+    ticket: Type.String(),
+    locationid: Type.Number(),
+  },
+  { additionalProperties: false }
+);
+type UpdateLocationRequest = Static<typeof updateLocationRequestSchema>;
 
-interface SteamSyncRequest {
-  steamusername: string;
-  snum: number;
-  s64: number;
-  ticket: string;
-  snums: string;
-  achstates: string;
-}
+const steamSyncRequestSchema = Type.Object(
+  {
+    steamusername: Type.String(),
+    snum: Type.Number(),
+    s64: Type.String(),
+    ticket: Type.String(),
+    snums: Type.String(),
+    achstates: Type.String(),
+  },
+  { additionalProperties: false }
+);
+type SteamSyncRequest = Static<typeof steamSyncRequestSchema>;
 
 export default async function routes(fastify: FastifyInstance) {
   fastify.post<{
     Body: SteamLoginRequest;
-  }>("/as_steamlogin/game_AttemptLoginSteamVerified.php", async (request) => {
-    const steamTicketResponse = await SteamUtils.verifySteamTicket(
-      request.body.ticket
-    );
-    const steamId = new SteamID(steamTicketResponse.response.params.steamid);
-    const steamUser = await SteamUtils.steamApi.getUserSummary(
-      steamId.getSteamID64()
-    );
+  }>(
+    "/as_steamlogin/game_AttemptLoginSteamVerified.php",
+    { schema: { body: steamLoginRequestSchema } },
+    async (request) => {
+      const steamTicketResponse = await SteamUtils.verifySteamTicket(
+        request.body.ticket
+      );
+      const steamId = new SteamID(steamTicketResponse.response.params.steamid);
+      const steamUser = await SteamUtils.steamApi.getUserSummary(
+        steamId.getSteamID64()
+      );
 
-    const user: User = await prisma.user.upsert({
-      where: { steamid64: steamId.getSteamID64() },
-      update: {
-        username: steamUser.nickname,
-        avatarUrl: steamUser.avatar.large,
-        avatarUrlMedium: steamUser.avatar.medium,
-        avatarUrlSmall: steamUser.avatar.small,
-      },
-      create: {
-        username: steamUser.nickname,
-        steamid64: steamId.getSteamID64(),
-        steamid32: steamId.accountid,
-        locationid: 1,
-        avatarUrl: steamUser.avatar.large,
-        avatarUrlMedium: steamUser.avatar.medium,
-        avatarUrlSmall: steamUser.avatar.small,
-      },
-    });
-
-    fastify.log.info("Game auth request for user %d", user.id);
-    return xmlBuilder.buildObject({
-      RESULT: {
-        $: {
-          status: "allgood",
+      const user: User = await prisma.user.upsert({
+        where: { steamid64: steamId.getSteamID64() },
+        update: {
+          username: steamUser.nickname,
+          avatarUrl: steamUser.avatar.large,
+          avatarUrlMedium: steamUser.avatar.medium,
+          avatarUrlSmall: steamUser.avatar.small,
         },
-        userid: user.id,
-        username: user.username,
-        locationid: user.locationid, // locationid is the n-th element in the location list you see when registering
-        steamid: user.steamid32, //SteamID32, not ID64
-      },
-    });
-  });
+        create: {
+          username: steamUser.nickname,
+          steamid64: steamId.getSteamID64(),
+          steamid32: steamId.accountid,
+          locationid: 1,
+          avatarUrl: steamUser.avatar.large,
+          avatarUrlMedium: steamUser.avatar.medium,
+          avatarUrlSmall: steamUser.avatar.small,
+        },
+      });
+
+      fastify.log.info("Game auth request for user %d", user.id);
+      return xmlBuilder.buildObject({
+        RESULT: {
+          $: {
+            status: "allgood",
+          },
+          userid: user.id,
+          username: user.username,
+          locationid: user.locationid, // locationid is the n-th element in the location list you see when registering
+          steamid: user.steamid32, //SteamID32, not ID64
+        },
+      });
+    }
+  );
 
   fastify.post<{
     Body: UpdateLocationRequest;
-  }>("/as_steamlogin/game_UpdateLocationid.php", async (request) => {
-    const steamTicketResponse = await SteamUtils.verifySteamTicket(
-      request.body.ticket
-    );
+  }>(
+    "/as_steamlogin/game_UpdateLocationid.php",
+    { schema: { body: updateLocationRequestSchema } },
+    async (request) => {
+      const steamTicketResponse = await SteamUtils.verifySteamTicket(
+        request.body.ticket
+      );
 
-    await prisma.user.update({
-      where: {
-        steamid64: steamTicketResponse.response.params.steamid,
-      },
-      data: {
-        locationid: +request.body.locationid,
-      },
-    });
-
-    return xmlBuilder.buildObject({
-      RESULT: {
-        $: {
-          status: "success",
-        },
-      },
-    });
-  });
-
-  fastify.post<{
-    Body: SteamSyncRequest;
-  }>("/as_steamlogin/game_SteamSyncSteamVerified.php", async (request) => {
-    const steamTicketResponse = await SteamUtils.verifySteamTicket(
-      request.body.ticket
-    );
-
-    const steamFriendList: number[] = request.body.snums.split("x").map(Number);
-
-    try {
       await prisma.user.update({
         where: {
           steamid64: steamTicketResponse.response.params.steamid,
         },
         data: {
-          rivals: {
-            connect: steamFriendList.map((steamid32) => ({ steamid32 })),
+          locationid: +request.body.locationid,
+        },
+      });
+
+      return xmlBuilder.buildObject({
+        RESULT: {
+          $: {
+            status: "success",
           },
         },
       });
-    } catch (e) {
-      if (
-        e instanceof Prisma.PrismaClientKnownRequestError &&
-        e.code === "P2025"
-      )
-        fastify.log.info("Adding friends: " + e.meta?.cause);
-      //this is gonna work trust me bro
-      else throw e;
     }
+  );
 
-    //Nowhere near close to the response the real server gives
-    //We do not need to care for this endpoint though, because neither will the client
-    return xmlBuilder.buildObject({
-      RESULT: {
-        $: {
-          status: "success",
+  fastify.post<{
+    Body: SteamSyncRequest;
+  }>(
+    "/as_steamlogin/game_SteamSyncSteamVerified.php",
+    { schema: { body: steamSyncRequestSchema } },
+    async (request) => {
+      const steamTicketResponse = await SteamUtils.verifySteamTicket(
+        request.body.ticket
+      );
+
+      const steamFriendList: number[] = request.body.snums
+        .split("x")
+        .map(Number);
+
+      try {
+        await prisma.user.update({
+          where: {
+            steamid64: steamTicketResponse.response.params.steamid,
+          },
+          data: {
+            rivals: {
+              connect: steamFriendList.map((steamid32) => ({ steamid32 })),
+            },
+          },
+        });
+      } catch (e) {
+        if (
+          e instanceof Prisma.PrismaClientKnownRequestError &&
+          e.code === "P2025"
+        )
+          fastify.log.info("Adding friends: " + e.meta?.cause);
+        //this is gonna work trust me bro
+        else throw e;
+      }
+
+      //Nowhere near close to the response the real server gives
+      //We do not need to care for this endpoint though, because neither will the client
+      return xmlBuilder.buildObject({
+        RESULT: {
+          $: {
+            status: "success",
+          },
         },
-      },
-    });
-  });
+      });
+    }
+  );
 }
