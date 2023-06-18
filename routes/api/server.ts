0b6132b68292dbb9e25693fe1df45c416daa3ec4
RubberDuckShobe <42943070+RubberDuckShobe@users.noreply.github.com>
2023-06-18 15:28:45 +0200

    add /server/getRadioSongs endpoint

--- routes/api/server.ts
+++ routes/api/server.ts
@@ -1,5 +1,14 @@
 import { FastifyInstance } from "fastify";
 import { prisma } from "../../util/db";
+import fs from "fs";
+
+type RadioEntry = {
+  wavebreakerId: number;
+  title: string;
+  artist: string;
+  externalUrl: string;
+  cgrFileUrl: string;
+};
 
 export default async function routes(fastify: FastifyInstance) {
   fastify.get("/api/server/getStats", async () => {
@@ -10,6 +19,32 @@ export default async function routes(fastify: FastifyInstance) {
       userCount,
       songCount,
       scoreCount,
+    };
+  });
+
+  fastify.get("/api/server/getRadioSongs", async () => {
+    const WavebreakerRadioConfig = JSON.parse(
+      fs.readFileSync(
+        globalThis.__basedir + "/config/wavebreaker_radio_entries.json",
+        "utf-8"
+      )
+    );
+    if (WavebreakerRadioConfig.availableSongs.length == 0) return { songs: [] };
+
+    const radioEntries: RadioEntry[] = WavebreakerRadioConfig.availableSongs;
+    const ids = radioEntries.map((entry) => entry.wavebreakerId);
+
+    const songs = await prisma.song.findMany({
+      where: {
+        id: {
+          in: ids,
+        },
+      },
+    });
+
+    return {
+      songs,
+      externalUrls: radioEntries.map((entry) => entry.externalUrl),
     };
   });
 }
--- routes/as1/radio.ts
+++ routes/as1/radio.ts
@@ -2,30 +2,34 @@ import { FastifyInstance } from "fastify";
 import fs from "fs";
 
 type RadioEntry = {
+  wavebreakerId: number;
   title: string;
   artist: string;
   externalUrl: string;
   cgrFileUrl: string;
 };
 
+
 export default async function routes(fastify: FastifyInstance) {
   //idk why this uses POST but it does
-  fastify.post(
-    "/as/asradio/game_asradiolist5.php",
-    async () => {
-      const WavebreakerRadioConfig = JSON.parse(fs.readFileSync(globalThis.__basedir + "/config/wavebreaker_radio_entries.json", "utf-8"));
-      if (WavebreakerRadioConfig.availableSongs.length == 0) return "";
-      
-      const separator = "-:*x-";
-      const radioEntries: RadioEntry[] = WavebreakerRadioConfig.availableSongs;
+  fastify.post("/as/asradio/game_asradiolist5.php", async () => {
+    const WavebreakerRadioConfig = JSON.parse(
+      fs.readFileSync(
+        globalThis.__basedir + "/config/wavebreaker_radio_entries.json",
+        "utf-8"
+      )
+    );
+    if (WavebreakerRadioConfig.availableSongs.length == 0) return "";
 
-      //Join every radio entry's properties (and the entries themselves) with the separator
-      const entriesString =
-        radioEntries
-          .map((entry) => Object.values(entry).join(separator))
-          .join(separator) + separator;
+    const separator = "-:*x-";
+    const radioEntries: RadioEntry[] = WavebreakerRadioConfig.availableSongs;
 
-      return entriesString;
-    }
-  );
+    //Join every radio entry's properties (and the entries themselves) with the separator
+    const entriesString =
+      radioEntries
+        .map((entry) => Object.values(entry).join(separator))
+        .join(separator) + separator;
+
+    return entriesString;
+  });
 }
